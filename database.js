bb9e1873e8faadaa48cefc00934474e9445eadf8
Murilo Ramos <muriloeduardo6@gmail.com>
2021-08-17 22:28:28 -0300

    mongodb connection

--- database.js
+++ database.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
+mongoose.Promise = global.Promise;
 mongoose
-  .connect("mongodb://localhost/ecommerce")
+  .connect("mongodb://localhost/ecommerce", { useMongoClient: true })
   .then(() => {
     console.log(`Server connected on mongodb://localhost/ecommerce`);
   })
